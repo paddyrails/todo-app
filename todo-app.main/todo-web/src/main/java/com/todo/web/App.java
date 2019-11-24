45a9b517d16d55d0ab90161692abcd75dc5f46e3
paddyrails <p_pillai3@hotmail.com>
2019-11-24 13:23:06 -0500

    initial project

---
+++ todo-app.main/todo-web/src/main/java/com/todo/web/App.java
@@ -0,0 +1,13 @@
+package com.todo.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
